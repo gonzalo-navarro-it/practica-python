0d89efab91ef4be9504a9f0b0aeab84f335fb49b
Gonzalo Navarro <gonzalonavaarro17@gmail.com>
2026-07-28 07:58:55 -0300

    Delete contar_multi.py

--- contar_multi.py
+++
@@ -1,9 +0,0 @@
-# Contar multiplos
-n = int(input("Ingrese un numero limite: "))
-contador = 0
-
-for i in range(1, n+1):
-    if i % 3 == 0:
-        contador = contador+1
-
-print("Cantiedad de multiplos de 3: ", contador)
