6cb18fa3e8f749f0f7d4e0606045d6c12c03d5e7
Gonzalo Navarro <gonzalonavaarro17@gmail.com>
2026-08-01 12:48:32 -0300

    Uso de funciones con suma

---
+++ funciones_suma.py
@@ -0,0 +1,12 @@
+# Uso de funciones con suma
+def result(x, y):
+    return (x+y)
+
+a = int(input("Ingrese primer numero: "))
+b = int(input("Ingrese segundo numero: "))
+
+r1 = result(a,b)
+print("Suma 1: ", r1)
+r2 = result(10, 20)
+print("Suma 2: ", r2)
+
